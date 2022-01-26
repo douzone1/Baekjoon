5a2d1dfcf5f9af3e3cc4ae4005f349f9ba926989
Donghun Lee <95545623+GIT-KINDMAN@users.noreply.github.com>
2022-01-26 11:01:16 +0900

    Update and rename Main.java to DH_10897.java
    통과

--- DH_10897.java
+++ DH_10897.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class Main {
+public class DH_10897 {
 
     public static void main(String args[]) throws Exception {
 
@@ -26,18 +26,38 @@ public class Main {
         }
 
     }
-} 
+}
 
-class Score implements Comparator<Score> {
+class Score implements Comparator<String[]> {
+    
+    @Override
+    public int compare(String[] s1, String[] s2) {
 
+        if (Integer.parseInt(s1[1]) == Integer.parseInt(s2[1])) {
+            if (Integer.parseInt(s1[2]) == Integer.parseInt(s2[2])) {
+                if (Integer.parseInt(s1[3]) == Integer.parseInt(s2[3])) {
+                    
+                    //4순위: 이름 오름차순
+                    return s1[0].compareTo(s2[0]);
+                }
+
+                //3순위: 수학 점수 = 내림차순
+                return Integer.compare(Integer.parseInt(s2[3]), Integer.parseInt(s1[3]));
+
+            }
+            //2순위: 영어 점수 = 오름차순
+            return Integer.compare(Integer.parseInt(s1[2]), Integer.parseInt(s2[2]));
+        }
+        //1순위: 국어점수 = 내림차순
+        return Integer.compare(Integer.parseInt(s2[1]), Integer.parseInt(s1[1]));
+    }
+
+    /*
     String name;
     String kor;
     String eng;
     String math;
 
-
-    
-    @Override
     public int compare(Score s1, Score s2) {
 
         if (Integer.parseInt(s1.kor) == Integer.parseInt(s2.kor)) {
@@ -58,8 +78,15 @@ class Score implements Comparator<Score> {
         //1순위: 국어점수 = 내림차순
         return Integer.compare(Integer.parseInt(s2.kor), Integer.parseInt(s1.kor));
     }
+    */
+    /*
+    int kor;
+    int eng;
+    int math;
+    String name;
     
-        /*
+    public int compare(Score s1, Score s2) {
+
         if (s1.kor == s2.kor) {
             if (s1.eng == s2.eng) {
                 if (s1.math == s2.math) {
