cd78ade5ac203caac1ddc571fcb6f833365fc696
Donghun Lee <95545623+GIT-KINDMAN@users.noreply.github.com>
2022-01-27 05:14:29 +0900

    Update and rename DH_10897.java to DH_10825.java

--- DH_10825.java
+++ DH_10825.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class DH_10897 {
+public class DH_10825 {
 
     public static void main(String args[]) throws Exception {
 
