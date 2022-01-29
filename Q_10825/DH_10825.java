f33df2398b5eb25861e2eb8fb28ea210d2133b22
Donghun Lee <95545623+GIT-KINDMAN@users.noreply.github.com>
2022-01-30 00:39:31 +0900

    LDH (22.01.26)

--- Q_10825/DH_10825.java
+++ Q_10825/DH_10825.java
@@ -3,7 +3,7 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-
+ 
 public class DH_10825 {
 
     public static void main(String args[]) throws Exception {
