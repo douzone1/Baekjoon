ba98f1bfeb1cac39ac36ab51190a1f66db1016ed
Donghun Lee <95545623+GIT-KINDMAN@users.noreply.github.com>
2022-01-30 00:05:56 +0900

    LDH

---
+++ Q_2442/LDH_2442.java
@@ -0,0 +1,28 @@
+package BOJ_2442;
+
+import java.util.Scanner;
+
+public class LDH_2442 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        pyramid(N);
+
+        sc.close();
+    }
+    
+    private static void pyramid(int N) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = N - i; j >= 1; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (i - 1) * 2 + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
