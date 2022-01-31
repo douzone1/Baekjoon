a6caf1fe68b66264317c379a0ea1a09f87fb274e
Limitiz <60959811+Limitiz@users.noreply.github.com>
2022-02-01 08:37:23 +0900

    LJY : refactoring

--- Q_1157/JY_1157.java
+++ Q_1157/JY_1157.java
@@ -6,27 +6,22 @@ public class JY_1157 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int word[] = new int[26];
-        int max = 0, second = 0;
+        int max = 0;
         int ans = 0;
         String s = br.readLine().toUpperCase();
 
-        for(int i=0; i<s.length(); i++){
-            int index = s.charAt(i)-'A';
-            word[index]++;
-        }
+        for(int i=0; i<s.length(); i++)
+            word[s.charAt(i)-'A']++;
 
         for(int i=0; i<26; i++){
             if(word[i] > max) {
                 max = word[i];
-                ans = i;
+                ans = (char)(i+'A');
             }
             else if(word[i] == max)
-                second = word[i];
+                ans = '?';
         }
-
-        if(max == second)
-            System.out.print("?");
-        else
-            System.out.print((char)(ans+'A'));
+        
+        System.out.print(ans);
     }
 }
