31c46d8aa67ee46d261bb4d5c58737a80f93d2e7
Limitiz <60959811+Limitiz@users.noreply.github.com>
2022-02-15 18:17:45 +0900

    LJY

---
+++ Q_1520/JY_1520.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class JY_1520 {
+    static int candy[][];
+    static int n,m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken())+1;
+        m = Integer.parseInt(st.nextToken())+1;
+
+        candy = new int[n][m];
+        for(int i=1; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=1; j<m; j++)
+                candy[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++)
+                candy[i][j] += Math.max(candy[i][j-1], Math.max(candy[i-1][j], candy[i-1][j-1]));
+        }
+        System.out.println(candy[n-1][m-1]);
+    }
+}
