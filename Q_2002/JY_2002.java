a0ca259c81310bef921ce063751893d096bd3317
Limitiz <60959811+Limitiz@users.noreply.github.com>
2022-02-18 17:27:57 +0900

    LJY : refactoring 필요없는 코드 제거

--- Q_2002/JY_2002.java
+++ Q_2002/JY_2002.java
@@ -4,27 +4,23 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 
 public class JY_2002 {
-    public static void main(String[] args) throws IOException{
+   public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         HashMap<String, Integer> map = new HashMap<>();
-        int fast = 0; boolean isFast;
+        int fast = 0;
 
         for(int i=0; i<n; i++) //터널에 들어간 순서
             map.put(br.readLine(), i);
 
         for(int i=0; i<n; i++) { //터널에서 나온 순서
             String car = br.readLine();
-            isFast = false;
             for(String key : map.keySet()) {
                 if (map.get(key) < map.get(car)) {
-                    isFast = true; //추월차량임
-                    map.replace(key, map.get(key) + 1); //i 앞의 차들은 순서가 하나씩 밀림
+                    fast++; break; //먼저 들어간 차량 있으면 추월차량
                 }
             }
             map.remove(car);
-
-            if(isFast) fast++;
         }
         System.out.println(fast);
     }
